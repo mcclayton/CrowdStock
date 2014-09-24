ba9f7fd44cbedf3e8f0a45f8e6e65bbb45b38768
Bill King <bill@billking.io>
2014-09-24 12:20:14 -0400

    Added base Admin controller

---
+++ CrowdStock/CrowdStock/Controllers/AdminController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PagedList;
+using CrowdStock.Models;
+
+namespace CrowdStock.Controllers
+{
+	[Authorize(Users="bill@billking.io")]
+	public class AdminController : Controller
+	{
+		CrowdStockDBContext db = new CrowdStockDBContext();
+
+		public ActionResult Index(int? page)
+		{
+			return View(db.Users.OrderBy(u => u.Id).ToPagedList(page ?? 1, 25));
+		}
+	}
+}
