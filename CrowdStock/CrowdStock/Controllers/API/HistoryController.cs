94f95af111f3bef530623b7f81bd2e111738a11f
Matthew Crabill <matthew.crabill@gmail.com>
2014-09-28 22:25:57 -0400

    added a History Controller

---
+++ CrowdStock/CrowdStock/Controllers/API/HistoryController.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using CrowdStock.Models;
+
+namespace CrowdStock.Controllers.API
+{
+    public class HistoryController : ApiController
+    {
+        private CrowdStockDBContext db = new CrowdStockDBContext();
+
+        // GET api/History
+        public IQueryable<History> GetHistories()
+        {
+            return db.Histories;
+        }
+
+        // GET api/History/5
+        [ResponseType(typeof(History))]
+        public async Task<IHttpActionResult> GetHistory(int id)
+        {
+            History history = await db.Histories.FindAsync(id);
+            if (history == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(history);
+        }
+
+        // PUT api/History/5
+        public async Task<IHttpActionResult> PutHistory(int id, History history)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != history.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(history).State = EntityState.Modified;
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!HistoryExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST api/History
+        [ResponseType(typeof(History))]
+        public async Task<IHttpActionResult> PostHistory(History history)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Histories.Add(history);
+            await db.SaveChangesAsync();
+
+            return CreatedAtRoute("DefaultApi", new { id = history.Id }, history);
+        }
+
+        // DELETE api/History/5
+        [ResponseType(typeof(History))]
+        public async Task<IHttpActionResult> DeleteHistory(int id)
+        {
+            History history = await db.Histories.FindAsync(id);
+            if (history == null)
+            {
+                return NotFound();
+            }
+
+            db.Histories.Remove(history);
+            await db.SaveChangesAsync();
+
+            return Ok(history);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool HistoryExists(int id)
+        {
+            return db.Histories.Count(e => e.Id == id) > 0;
+        }
+    }
+}
