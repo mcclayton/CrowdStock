d486fa9986be811fae4bb0050a8d74dc64a1fb20
Matthew Crabill <matthew.crabill@gmail.com>
2014-10-05 01:37:34 -0400

    added stuff

---
+++ CrowdStock/CrowdStockDb/Quote.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CrowdStockDb
+{
+    public class Quote
+    {
+        public string Date {get; set;}
+        public float Open { get; set; }
+        public float High { get; set; }
+        public float Low { get; set; }
+        public float Close { get; set; }
+        public int Volume { get; set; }
+        public float Adj_Close { get; set; }
+    }
+}
